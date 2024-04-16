521913519f3cdd1a34b6b619dc20f95272a665db
山本駿 <yamamotoshun@yamamotoshunnoMacBook-Pro.local>
2024-04-17 03:38:58 +0900

    add update function for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
   rescue_from ActionController::ParameterMissing, with: :handle_parameter_missing
-  rescue_from ActiveRecord::RecordInvalid, with: :handle_parameter_missing
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_validation_error
 
   def show
   end
@@ -16,16 +17,35 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(update_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def user_params
     params.require(:todo).permit(:username, :email, :password)
   end
 
+  def update_params
+    params.require(:todo).permit(:email, :password)
+  end
+
   def handle_parameter_missing(e)
+    render json: { message: e.message }, status: 400
+  end
+
+  def handle_validation_error(e)
     render json: { message: e }, status: 400
   end
 end
