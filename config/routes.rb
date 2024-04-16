4c46da744b6e843227c3572dadb3cfb671427a9a
山本駿 <yamamotoshun@yamamotoshunnoMacBook-Pro.local>
2024-04-17 03:24:07 +0900

    add resources for users and todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :create, :update, :destroy]
+  resources :todos, only: [:index, :show, :create, :update, :destroy]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
